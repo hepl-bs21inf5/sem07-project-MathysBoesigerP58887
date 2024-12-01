fbd6d813c582f8206f142dde14ed49a43eda665b
Mathys Boesiger <mathys.boesiger@etu.hepl.ch>
2024-12-01 10:53:42 +0100

    correction Trivia

--- src/components/QuizTrivia.vue
+++ src/components/QuizTrivia.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import QuestionRadio from '@/components/QuestionRadio.vue'
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 
-// Déclaration d'une liste réactive pour stocker les questions
 const questions = ref<
   {
     question: string
@@ -10,31 +9,27 @@ const questions = ref<
     incorrect_answers: string[]
   }[]
 >([])
-const answers = reactive<{ [key: number]: string | null }>({})
 
-// Appel API pour récupérer une liste de questions à choix multiples
 fetch('https://opentdb.com/api.php?amount=10&type=multiple')
   .then((response) => response.json())
-  .then((data) => (questions.value = data.results)) // Stocke les questions récupérées dans la liste réactive
+  .then((data) => (questions.value = data.results))
 </script>
 
 <template>
   <form>
-    <!-- Fait une boucle sur chaque question afin de séparer la question, la réponses et les choix possibles -->
     <QuestionRadio
       v-for="(question, index) in questions"
       :id="index.toString()"
       :key="index"
-      v-model="answers[index]"
       :text="question.question"
       :options="[
-        // Formatage des options pour QuestionRadio
         { value: question.correct_answer, text: question.correct_answer },
         ...question.incorrect_answers.map((answer) => ({
           value: answer,
           text: answer,
         })),
       ]"
+      :answer="question.correct_answer"
     />
   </form>
 </template>
