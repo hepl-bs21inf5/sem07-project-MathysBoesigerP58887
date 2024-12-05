83b228e56cb9b248fe70da153a2a6c6e745a9106
Mathys Boesiger <mathys.boesiger@etu.hepl.ch>
2024-12-05 14:00:01 +0100

    modifier homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,16 @@ import QuizForm from '@/components/QuizForm.vue'
 </script>
 
 <template>
-  <div class="container mt-3">
-    <h1>Quiz</h1>
-    <QuizForm />
+  <div class="container-fluid mt-3">
+    <!-- container-fluid permet de s'étendre sur toute la largeur -->
+    <div class="row justify-content-center">
+      <!-- Centrer le contenu sur toutes les tailles d'écran -->
+      <div class="col-12 col-md-8 col-lg-6">
+        <!-- Ajuste la largeur selon la taille de l'écran -->
+        <h1 class="text-center mb-4">Quiz</h1>
+        <!-- Titre centré -->
+        <QuizForm />
+      </div>
+    </div>
   </div>
 </template>
